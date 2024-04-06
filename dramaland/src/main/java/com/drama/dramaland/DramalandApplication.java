cbbcde5dec76ba4a28eb8abeda4c26de2adebf03
unknown <suprajach.2677@gmail.com>
2024-04-06 16:11:41 +0530

    Springboot setup

---
+++ dramaland/src/main/java/com/drama/dramaland/DramalandApplication.java
@@ -0,0 +1,13 @@
+package com.drama.dramaland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramalandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramalandApplication.class, args);
+	}
+
+}
---
+++ dramaland/src/main/java/com/drama/dramaland/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.drama.dramaland.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String getDemo(){
+        return "Demo Drama";
+    }
+
+}
