ed7ce74e52980055386b427440be097cfb2f23e0
unknown <suprajach.2677@gmail.com>
2024-04-14 19:25:13 +0530

    CORS changes

--- dramaland/src/main/java/com/drama/dramaland/controller/CountryController.java
+++ dramaland/src/main/java/com/drama/dramaland/controller/CountryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/drama/api")
 public class CountryController {
     @Autowired
